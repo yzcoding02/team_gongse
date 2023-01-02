e1f377820148fb12dde30da936cc4569f0e8c557
choiminseong <choiminseong@.com>
2023-01-02 18:33:41 +0900

    python 기본

---
+++ Minsung/python_HW/Day1_hw.py
@@ -0,0 +1,22 @@
+# 출력은 print
+# + 더하기
+# - 빼기
+# * 곱하기
+# / 나누기
+# ** 거듭제곱
+# // 몫
+# % 나머지
+
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(1/3)
+print(2**2)
+print(3**3)
+print(orange//apple)
+print(10//3)
+print(10%3)
+print(orange%apple)
