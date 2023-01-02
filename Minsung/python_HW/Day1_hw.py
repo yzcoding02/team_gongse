d9ab956acab9669324f661b92c8f76a366aa323f
choiminseong <choiminseong@.com>
2023-01-02 21:10:13 +0900

    복습

--- Minsung/python_HW/Day1_hw.py
+++ Minsung/python_HW/Day1_hw.py
@@ -7,16 +7,23 @@
 # // 몫
 # % 나머지
 
-apple=10
-orange=20
-print(orange+apple)
-print(orange-apple)
-print(orange*apple)
-print(orange/apple)
-print(1/3)
-print(2**2)
-print(3**3)
-print(orange//apple)
-print(10//3)
-print(10%3)
-print(orange%apple)
+# apple=10
+# orange=20
+# print(orange+apple)
+# print(orange-apple)
+# print(orange*apple)
+# print(orange/apple)
+# print(1/3)
+# print(2**2)
+# print(3**3)
+# print(orange//apple)
+# print(10//3)
+# print(10%3)
+# print(orange%apple)
+
+print(5**2)
+print(2**5)
+print(2**10)
+
+print(100//3)
+print(100%3)
