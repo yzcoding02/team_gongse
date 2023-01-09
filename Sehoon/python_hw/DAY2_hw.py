e78da008cb9e22aab0972389177a804ebfcb7f90
hssh0413 <bockman822@gmail.com>
2023-01-09 21:05:29 +0900

    DAY2_hw

---
+++ Sehoon/python_hw/DAY2_hw.py
@@ -0,0 +1,34 @@
+# 1
+# for i in range(1,10):
+#     if i%2==1:
+#         print(i)
+# 2
+# for i in range(1,11):
+#     if i%3!=0:
+#         print(i)
+# 3
+# for i in range(50,101):
+#     if i%3==0:
+#         if i%4==0:
+#             print(i)
+# 4
+# a=1
+# for i in range(1,21):
+#     if i%7==0:
+#         a*=i
+# print(a)
+# 5
+# a=0
+# for i in range(50,61):
+#     if i%4!=0:
+#         a+=i
+# print(a)
+# 6
+# a=1
+# b=1
+# for i in range(1,101):
+#     if i%2==0:
+#         a+=i
+#     if i%2!=0:
+#         b+=i
+# print(a-b)
