5d5cb257d97b5dd5921d8cf7d0bbd0f937ec4b3e
hssh0413 <bockman822@gmail.com>
2023-03-21 02:07:19 +0900

    Revert "3/20 숙제-그래프 활용 ㅠ"
    This reverts commit 8d37b5a14c5de85b325a346436117fa523104651.

--- Sehoon/python_hw/DAY4_hw.py
+++ Sehoon/python_hw/DAY4_hw.py
@@ -1,19 +1,7 @@
 # Q-두수의 최소공배수를 구하는 함수
-# def L(a,b):
-#     for i in range(max(a,b),(a*b)+1):
-#         if i%a==0 and i%b==0:
-#             print(i)
-#             break
-# L(12,60)
-# def GCD(a,b,c):
-#     for i in range(min(a,b,c),0,-1):
-#         if a%i==0 and b%i==0 and c%i==0:
-#             return print(i)
-# GCD(4,6,10)
-# GCD(10,15,20)
-def LCM(a,b,c):
-    for i in range(min(a,b,c),0,-1):
-        if a%i==0 and b%i==0 and c%i==0:
-            return print(i)
-LCM(4,6,10)
-LCM(10,15,20)
+def 최소공배수(a,b):
+    for i in range(max(a,b),(a*b)+1):
+        if i%a==0 and i%b==0:
+            print(i)
+            break
+최소공배수(12,60)
--- Sehoon/python_hw/DAY5_hw.py
+++
@@ -1,5 +0,0 @@
-# Q.구구단 출력
-# n=[]
-# for i in n:
-#     for j in range(1,10):
-#         print(i,"*",j,"=",i*j)
